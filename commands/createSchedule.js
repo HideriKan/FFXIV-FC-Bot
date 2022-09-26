e5adf65a3bf57fe12527eeb6deafbc7c5f72d11c
Max <keinikein@gmail.com>
2022-09-26 21:09:49 +0200

    fix: saves the times correctly now

--- commands/createSchedule.js
+++ commands/createSchedule.js
@@ -1,14 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-// const { Interaction } = require('discord.js');
+const { Interaction } = require('discord.js');
 const RaidWeek = require('../RaidWeek');
-const getStartingDay = require('../utility');
+const { getStartingDay } = require('../utility');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('createschedule')
 		.setDescription('Creates a new Schedule for the raid command to be displayed')
-		.addBooleanOption( option => option.setName('next').setDescription('Edit current week?').setRequired(true))
-		.addStringOption( option => 
+		.addBooleanOption(option => 
+			option.setName('next')
+				.setDescription('Edit current week?')
+				.setRequired(true))
+		.addStringOption(option =>
 			option.setName('batch')
 				.setDescription('Add week times in Tu/We/Th/Fr/Sa/Su/Mo')
 				.setRequired(true))
@@ -19,10 +22,28 @@ module.exports = {
 	 * @param {Interaction} interaction
 	 */
 	async execute(interaction) {
+		if (interaction.channel.type == 'DM') {
+			interaction.reply({
+				content: 'Dont use this command in the dms for the scheduled guild events',
+				ephemeral: true
+			});
+			return;
+		}
+
+
+		if (interaction.memberPermissions.equals('MANAGE_EVENTS')) {
+			interaction.reply({
+				content: 'No permission',
+				ephemeral: true
+			});
+			return;
+		}
+
+
 		const raidWeek = new RaidWeek();
 		const editNext = interaction.options.getBoolean('next');
 		const batch = interaction.options.getString('batch');
-		const batchArr = batch.split('/');		
+		const batchArr = batch.split('/');
 
 		if (editNext) // when the user is chosing a next week
 			raidWeek.startingWeekDay = getStartingDay(true);
