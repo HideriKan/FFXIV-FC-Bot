c3aed38c76f7f85a064523205fb9581323db1f26
Maximilian Eric Eckelmann <eckelmannmaxeric@gmail.com>
2022-10-21 14:43:22 +0200

    add: skeleton of loot cmd

--- commands/loot.js
+++ commands/loot.js
@@ -1,39 +1,60 @@
-const { SlashCommandBuilder, ActionRowBuilder } = require('@discordjs/builders');
-const { ButtonStyle } = require('discord-api-types');
-
-const itemTypes = [
-    { name: 'Gear', value: 'gear' },
-    { name: 'Weapon', value: 'weap' },
-    { name: 'Body', value: 'body' },
-    { name: 'Tome Weapon', value: 'tomeWeap' },
-    { name: 'Tome Weapon Upgrade', value: 'tomeUp' },
-    { name: 'Tome Gear Upgrade', value: 'gearUp' },
-    { name: 'Tome Accessory Upgrade', value: 'accUp' },
-]
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Loot')
-        .setDescription('Loot manager')
-        .addSubcommand(subcmd => subcmd
-            .setName('Got')
+        .setName('loot')
+        .setDescription('Loot tracker')
+        .addSubcommand(subcmd => subcmd // cmd: got, opt: [user, type]
+            .setName('got')
             .setDescription('Distrubte loot for a raid member')
-            .addUserOption(opt => opt.setName('Member').setDescription('Role which got the loot').setRequired(true))
-            .addStringOption(opt => opt.setName('Type').setDescription('Item Type').setChoices(itemTypes).setAutocomplete(true).setRequired(true))
+            .addUserOption(opt => opt
+                .setName('user')
+                .setDescription('who got the loot')
+                .setRequired(true))
+            .addStringOption(opt => opt
+                .setName('type')
+                .setDescription('Item Type')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Gear', value: 'gear' },
+                    { name: 'Weapon', value: 'weap' },
+                    { name: 'Body', value: 'body' },
+                    { name: 'Tome Weapon', value: 'tomeWeap' },
+                    { name: 'Tome Weapon Upgrade', value: 'tomeUp' },
+                    { name: 'Tome Gear Upgrade', value: 'gearUp' },
+                    { name: 'Tome Accessory Upgrade', value: 'accUp' }
+                )
+            )
         )
-        .addSubcommand(subcmd => subcmd
-            .setName('Show')
+        .addSubcommand(subcmd => subcmd // cmd: show, opt: type
+            .setName('show')
             .setDescription('Show who can roll / priority')
-            .addStringOption(opt => opt.setName('Type').setDescription('Item Type').setChoices(itemTypes).setAutocomplete(true).setRequired(true))
-
+            .addStringOption(opt => opt
+                .setName('type')
+                .setDescription('Item Type')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Gear', value: 'gear' },
+                    { name: 'Weapon', value: 'weap' },
+                    { name: 'Body', value: 'body' },
+                    { name: 'Tome Weapon', value: 'tomeWeap' },
+                    { name: 'Tome Weapon Upgrade', value: 'tomeUp' },
+                    { name: 'Tome Gear Upgrade', value: 'gearUp' },
+                    { name: 'Tome Accessory Upgrade', value: 'accUp' }
+                )
+            )
         )
     ,
     /**
     * @param {Interaction} interaction
      */
     async execute(interaction) {
+        // await interaction.deferReply();
         const cmd = interaction.options.getSubcommand();
-        const btnY = new ActionRowBuilder()
+        const type = interaction.options.getString('type');
+        let content = `The ${cmd} with options: "${type}" is a WIP command`;
+
+        const row = new ActionRowBuilder()
             .addComponents(new ButtonBuilder()
                 .setCustomId('yes')
                 .setLabel('Yes')
@@ -43,10 +64,10 @@ module.exports = {
                 .setCustomId('no')
                 .setLabel('No')
                 .setStyle(ButtonStyle.Danger)
-        );
+            );
 
         interaction.reply({
-            content: `The ${cmd} command is WIP`,
+            content: content,
             components: [row],
             ephemeral: true
         });
