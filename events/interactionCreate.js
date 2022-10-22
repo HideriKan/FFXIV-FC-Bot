7c372bca4a392530a7978944b94cb77a7bdb7284
Max <keinikein@gmail.com>
2022-10-22 15:41:51 +0200

    add: button interactoin
    (cherry picked from commit 4963b25a822bee31164330869a6e0ad146b6134d)

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,31 @@
+const RaidWeek = require('../Classes/RaidWeek');
+const { createScheduledEvents, assingToMember } = require('../utility');
+
 module.exports = {
 	name: 'interactionCreate',
 	/**
 	 * 
-	 * @param {Interaction} interaction 
+	 * @param {import("discord.js").Interaction} interaction 
 	 */
 	async execute(interaction) {
-		if (!interaction.isCommand()) return;
-
-		const command = interaction.client.commands.get(interaction.commandName);
-		if (!command) return;
-
 		try {
-			await command.execute(interaction);
+
+			if (interaction.isButton()) {
+				if (interaction.customId === 'savage' || interaction.customId === 'ult')
+					await createScheduledEvents(interaction, new RaidWeek());
+				if (interaction.customId === 'yes')
+					await assingToMember(interaction);
+				else if (interaction.customId === 'no')
+					interaction.update({ content: 'Command has been canceled', components: [] });
+			}
+
+			else if (interaction.isCommand()) {
+				const command = interaction.client.commands.get(interaction.commandName);
+				if (!command) return;
+
+				await command.execute(interaction);
+			}
+
 		} catch (error) {
 			console.error(error);
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
