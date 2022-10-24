d4185f0cf8aa8447dbb851a54bf16de75c5fa269
Maximilian Eric Eckelmann <eckelmannmaxeric@gmail.com>
2022-10-24 16:52:10 +0200

    fix: moved code snipped into utility space
    get time from string will be used in more than one place now

--- utility.js
+++ utility.js
@@ -1,4 +1,5 @@
 const { GuildScheduledEventPrivacyLevel, GuildScheduledEventEntityType } = require('discord.js');
+const RaidDay = require('./Classes/RaidDay');
 
 /**
  * Returns the day of the reset in FFXIV (Tuesday)
@@ -15,6 +16,46 @@ function getStartingDay(isNextWeek = false) { // ++ or --
 	return now;
 } // End of getStartingDay
 
+/**
+ * Will convert any time format be it XX / XXXX / XX:XX or empty
+ * @param {String} timeStr user input from command
+ * @param {Date} raidDate date of the raidDay
+ * @returns {RaidDay} adjusted raidDay
+ */
+function getRaidDayFromString(timeStr, raidDate) {
+	const ownDate = new Date(raidDate);
+	const rDay = new RaidDay(ownDate);
+	let isDateSet = false;
+	timeStr.trim();
+
+	if (!isNaN(timeStr) || timeStr.includes(':')) {
+
+		if (timeStr.length === 2 && timeStr != '00') {
+			ownDate.setUTCHours(timeStr, 0);
+			isDateSet = true;
+
+		} else if (timeStr.length === 4) {
+			ownDate.setUTCHours(timeStr.substring(0, 2), timeStr.substring(2));
+			isDateSet = true;
+
+		} else if (timeStr.length === 5) {
+			let arrTimes = timeStr.split(':');
+
+			if (!isNaN(arrTimes[0]) && !isNaN(arrTimes[1])) {
+				ownDate.setUTCHours(arrTimes[0], arrTimes[1]);
+				isDateSet = true;
+			}
+		}
+
+		if (isDateSet) {
+			rDay.isRaid = true;
+			rDay.startTime = ownDate.toJSON();
+		}
+
+	}
+
+	return rDay;
+}
 
 const staticChannels = [{ id: '968545420198416397', type: 'ult' }, { id: '1012614749378326609', type: 'savage' }];
 /**
@@ -60,6 +101,7 @@ async function assingToMember(interaction) {
 
 module.exports = {
 	getStartingDay,
+	getRaidDayFromString,
 	createScheduledEvents,
 	assingToMember
 }; 
