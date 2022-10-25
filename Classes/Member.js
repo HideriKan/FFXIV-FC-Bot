c06ef695668cb21229570c6f49abc8f8e04aaf79
Maximilian Eric Eckelmann <eckelmannmaxeric@gmail.com>
2022-10-25 16:57:51 +0200

    add: member attributes

--- Classes/Member.js
+++ Classes/Member.js
@@ -1,7 +1,18 @@
 class Member {
 	constructor(name, id) {
-		this.name = name;
-		this.id = id;
+		this.name = name; // display name
+		this.id = id; // unique discord snowflake id
+		this.hasBiS = false; // might not use
+		this.priority = 0;
+		this.totalGear = 0;
+		this.hasWeapon = false;
+		this.hasBody = false;
+		this.hasTomeWeap = false;
+		this.hasTimeWeapUp = false;
+		this.TotalGearUp = 0;
+		this.TotalAccUp = 0;
+	}
+
 	}
 }
 
