c893d787afdae267f00583201de7c60781e3910e
Maximilian Eric Eckelmann <eckelmannmaxeric@gmail.com>
2022-10-25 16:58:50 +0200

    add: static methods for read and write

--- Classes/Member.js
+++ Classes/Member.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+
 class Member {
 	static fileName = './MemberLoot.json';
+
 	constructor(name, id) {
 		this.name = name; // display name
 		this.id = id; // unique discord snowflake id
@@ -14,6 +17,29 @@ class Member {
 		this.TotalAccUp = 0;
 	}
 
+	/**
+	 * will write the passed data into the json
+	 * @param {Array} data an Array of Member
+	 */
+	static saveMembers(data) {
+		try {
+			fs.writeFileSync(this.fileName, JSON.stringify(data, null, 2));
+		} catch (err) {
+			if (err) return console.error(err);
+		}
+	}
+
+	/**
+	 * reads the json file and returns the found members
+	 * @returns {Array} array of members
+	 */
+	static getAllMembers() {
+		try {
+			return JSON.parse(fs.readFileSync(this.fileName, 'utf8'));
+		} catch (error) {
+			console.error(err);
+			return new Array;
+		}
 	}
 }
 
